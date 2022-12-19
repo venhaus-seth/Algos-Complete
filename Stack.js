302cf3de6d8765a07fe79bea97a089712c17ebaa
seth venhaus <venhaus.seth@gmail.com>
2022-12-19 08:46:37 -0800

    Stack data structure

---
+++ Stack.js
@@ -0,0 +1,81 @@
+/**
+ * Class to represent a stack using an array to store the stacked items.
+ * Follows a LIFO (Last In First Out) order where new items are stacked on
+ * top (back of array) and removed items are removed from the top / back.
+ */
+class Stack {
+    /**
+     * The constructor is executed when instantiating a new Stack() to construct
+     * a new instance.
+     * @returns {Stack} This new Stack instance is returned without having to
+     *    explicitly write 'return' (implicit return).
+     */
+    constructor() {
+      this.items = [];
+    }
+  
+    /**
+     * Adds a new given item to the top / back of this stack.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @param {any} item The new item to be added to the top / back.
+     * @returns {number} The new length of this stack.
+     */
+    push(item) {
+      return this.items.push(item)
+    }
+  
+    /**
+     * Removes the top / last item from this stack.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {any} The removed item or undefined if this stack was empty.
+     */
+    pop() {
+      return this.items.pop()
+    }
+  
+    /**
+     * Retrieves the top / last item from this stack without removing it.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {any} The top / last item of this stack.
+     */
+    peek() {
+      return this.items[this.items.length-1]
+    }
+  
+    /**
+     * Returns whether or not this stack is empty.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {boolean}
+     */
+    isEmpty() {
+      return this.items.length == 0 ? true : false 
+    }
+  
+    /**
+     * Returns the size of this stack.
+     * - Time: O(1) constant.
+     * - Space: O(1) constant.
+     * @returns {number} The length.
+     */
+    size() {
+      return this.items.length
+    }
+}
+
+
+newStack = new Stack
+newStack.push(3)
+newStack.push(4)
+newStack.push(6)
+newStack.push(1)
+console.log(newStack.peek())
+console.log(newStack.push(4))
+console.log(newStack.peek())
+console.log(newStack.pop())
+console.log(newStack.peek())
+console.log(newStack.isEmpty())
+console.log(newStack.size())
