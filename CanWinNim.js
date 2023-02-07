39794b92e7613beb9dd87b090a987efbb68a2bfb
seth venhaus <venhaus.seth@gmail.com>
2023-02-07 09:55:18 -0800

    added

---
+++ CanWinNim.js
@@ -0,0 +1,16 @@
+var canWinNim = function(n) {
+    return n % 4 === 0 ? false : true
+};
+
+n1 = 1
+n2 = 2
+n3 = 3
+n4 = 4
+n5 = 10
+n6 = 12
+console.log(canWinNim(n1))
+console.log(canWinNim(n2))
+console.log(canWinNim(n3))
+console.log(canWinNim(n4))
+console.log(canWinNim(n5))
+console.log(canWinNim(n6))
