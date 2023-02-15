3d8b9dd4e99784e01ec92cd6c869e0c2d67d3a39
seth venhaus <venhaus.seth@gmail.com>
2023-02-15 09:43:13 -0800

    added

---
+++ ValidPerfectSquare.js
@@ -0,0 +1,15 @@
+var isPerfectSquare = function(num) {
+    
+    return (num ** .5 % 1 === 0)
+};
+
+num1 = 16
+num2 = 14
+num3 = 25
+num4 = 200
+num5 = 225
+console.log(isPerfectSquare(num1))
+console.log(isPerfectSquare(num2))
+console.log(isPerfectSquare(num3))
+console.log(isPerfectSquare(num4))
+console.log(isPerfectSquare(num5))
